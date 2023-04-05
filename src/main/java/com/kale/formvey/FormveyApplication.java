9b86f51ddc57a89b2ab852c4ceadd7a796bac3a3
chkang13 <chkang31@naver.com>
2023-04-06 02:20:21 +0900

    testing

---
+++ src/main/java/com/kale/formvey/FormveyApplication.java
@@ -0,0 +1,15 @@
+package com.kale.formvey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+public class FormveyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormveyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kale/formvey/FormveyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kale.formvey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormveyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
