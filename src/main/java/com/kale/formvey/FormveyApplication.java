e46c0afb4eb639fac043c2936f2162b16a5dc904
harry <lsh929500@gmail.com>
2023-05-23 23:05:40 +0900

    formvey push test

--- src/main/java/com/kale/formvey/FormveyApplication.java
+++ src/main/java/com/kale/formvey/FormveyApplication.java
@@ -14,6 +14,7 @@ import java.util.Scanner;
 @EnableJpaAuditing
 @EnableScheduling
 class FormveyApplication {
+	// ci/cd test
 	public static void main(String[] args) {
 		SpringApplication.run(FormveyApplication.class, args);
 	}
